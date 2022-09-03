36afda50caa2a64958e9a1fd0731ff9ae19fedc1
Aratrika Mallick <95929714+Aratrika-2003@users.noreply.github.com>
2022-09-03 12:28:44 +0530

    Create array.h

---
+++ array.h
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class array
+{
+    int *arr;
+    int size;
+    
+    public:
+        array();
+        array(int size);
+        void take_input();
+        void display();
+        array add(array temp);
+};
+
+class matrix
+{
+    int **arr,*res;
+    int row;
+    int col;
+
+    public:
+        matrix();
+        matrix(int row,int col)
+        void takeinp();
+        matrix add(matrix mat)
+        void displaymat();
+};
