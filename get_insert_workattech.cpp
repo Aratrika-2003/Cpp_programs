11dccd31925dd3aae7d781b84d60edbdb914de7a
Aratrika Mallick <95929714+Aratrika-2003@users.noreply.github.com>
2022-12-06 11:43:07 +0530

    Create get_insert_workattech.cpp

---
+++ get_insert_workattech.cpp
@@ -0,0 +1,12 @@
+int getInsertPosition(vector<int> &arr, int key) 
+{
+    // add your logic here
+	for(int i = 0; i < arr.size(); i++)
+	{
+		if(arr[i] == key)
+			return i;
+		else if(arr[i] > key)
+			return i;
+	}
+	return arr.size();
+}
